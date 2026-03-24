397a2780ea9128b9d3b1fb9381e742bd68f85d12
Vasyl <vasja112@gmail.com>
2026-03-24 16:52:31 +0200

    fix TIME_ZONE

--- ticketing-project/config/settings.py
+++ ticketing-project/config/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'uk'
-TIME_ZONE = 'Europe/Kiev'
+TIME_ZONE = 'Europe/Kyiv'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
